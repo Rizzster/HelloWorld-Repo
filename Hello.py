918dc37369e3bc578b214af00243a7de9c85897e
Rizzster <122623887+Rizzster@users.noreply.github.com>
2023-01-13 09:00:32 -0600

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,24 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fswiss\fcharset0 ArialMT;}
+{\colortbl;\red255\green255\blue255;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30\fsmilli15139 \cf0 \cb2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec3 def main():\strokec0 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \strokec0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30\fsmilli15139 \cf0 \strokec3 	\
+print(\'93Hello World!\'94)\strokec0 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\fs32 \cf0 \strokec0 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30\fsmilli15139 \cf0 \strokec3 main()\strokec0 \
+}
